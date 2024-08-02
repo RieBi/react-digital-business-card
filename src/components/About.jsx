9d9e3bd639a1bb4fad97d50f9254868329ba1d91
RieBi <42877671+RieBi@users.noreply.github.com>
2024-08-02 14:37:42 +0200

    Add About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,12 @@
 export default function About() {
     return (
-        <p>About is here</p>
+        <div className="about">
+            <h3>About</h3>
+            <p>
+                I am a frontend developer with a particular interest to cats.
+                I try to keep up with the world and make myself better, learning new things and trying new stuff.
+                Right now I'm learning React so here it is.
+            </p>
+        </div>
     )
 }
