f88b0cbc7741cf28d08c2125ecd152be07d964ac
RieBi <42877671+RieBi@users.noreply.github.com>
2024-08-02 15:22:50 +0200

    Add Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,17 @@
+import twitter from "../assets/social-twitter.svg"
+import facebook from "../assets/social-facebook.svg"
+import instagram from "../assets/social-instagram.svg"
+import github from "../assets/social-github.svg"
+
 export default function Footer() {
     return (
-        <p>Footer</p>
+        <div className="footer">
+            <div className="socials">
+                <img src={twitter} />
+                <img src={facebook} />
+                <img src={instagram} />
+                <img src={github} />
+            </div>
+        </div>
     )
 }
