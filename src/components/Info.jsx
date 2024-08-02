5fddded4637b54f94a9ecb3db9e67c473d2205d9
RieBi <42877671+RieBi@users.noreply.github.com>
2024-08-02 14:27:19 +0200

    Add Info section

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,28 @@
 import cat from '../assets/cat.jpg'
+import mail from '../assets/mail.svg'
+import linkedin from '../assets/linkedin.svg'
 
 export default function Info() {
     return (
-        <p>Info</p>
+        <>
+            <img src={cat} className="portrait" />
+            <h2 className="title">Sviatoslav Zubar</h2>
+            <p className="subtitle">Frontend Developer</p>
+            <p className="website">example.org</p>
+            <div className="buttons">
+                <a href="mailto:email@example.org">
+                    <button type="button" className="button email-button">
+                        <img src={mail} className="icon" />
+                        <span>Email</span>
+                    </button>
+                </a>
+                <a href="https://linkedin.com">
+                    <button type="button" className="button linkedin-button">
+                        <img src={linkedin} className="icon" />
+                        <span>LinkedIn</span>
+                    </button>
+                </a>
+            </div>
+        </>
     )
 }
